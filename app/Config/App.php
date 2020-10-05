551aae01e5237f2db67637a2229c86fc61d7888c
Xavier Perez <xmadmax@gmail.com>
2020-10-05 12:39:19 +0200

    Initial commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://workspace.local/CodeIgniter4/CI4DEV/public/';
+	public $baseURL = '';
 
 	/*
 	|--------------------------------------------------------------------------
