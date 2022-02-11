a2a7fe1de888a776d25d4ef5cadd2d312be0c7b0
Xavier Perez <xavier.perez@levelprograms.com>
2022-02-11 13:30:24 +0100

    Test
    Test

---
+++ app/Modules/Home2/Language/es/General.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "myprofile" => "Mis datos",
+    "myprofile-title" => "Mi cuenta",
+    "myprofile-desc" => "Entra para revisar o cambiar tus datos de usuario",
+    "login" => "Entrar",
+    "register" => "Regístrate",
+    "logout" => "Salir",
+    "si" => "Sí",
+    "no" => "No",
+    "calcular.button" => "Calcular",
+    "cancelar.button" => "Cancelar",
+    "continuar.button" => "Continuar",
+    "aceptar.button" => "Aceptar",
+    "volver.button" => "Volver",
+    "atention-title" => "Atención",
+    "campoObligatorio" => "Este campo es obligatorio",
+    "borrarregistro" => "Está seguro de borrar este registro ?",
+    "registroborrado" => "Registro borrado",
+    "espereporfavor" => "Un momento, por favor"
+];
