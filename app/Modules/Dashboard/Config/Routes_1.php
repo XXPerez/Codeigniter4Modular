144bdf2ddc35b8d01afa2492a1349c56ccb943f6
Xavier Perez <xavier.perez@levelprograms.com>
2022-02-11 13:26:37 +0100

    test
    test

---
+++ app/Modules/Dashboard/Config/Routes_1.php
@@ -0,0 +1,6 @@
+<?php
+namespace Home\Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = \Config\Services::routes();
+$routes->add('home', 'Home\Controllers\Home', ['filter' => 'auth']);
