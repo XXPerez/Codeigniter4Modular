c601ca24d2f6aee9373c9022cd3344aedc9febba
Xavier Perez <xavier.perez@levelprograms.com>
2022-02-21 17:19:28 +0100

    Update UsersModel.php

--- app/Modules/Users/Models/UsersModel.php
+++ app/Modules/Users/Models/UsersModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class UsersModel extends Model {
 
     protected $table = 'users';
+    
     protected $primaryKey = 'id';
     protected $returnType = 'object';
     protected $useSoftDeletes = false;
